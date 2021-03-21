281cae7313d46e41087ddd16b82ea08377564df6
Ecospell <foretoo@yandex.ru>
2021-03-21 04:20:29 +0300

    refactored

--- p5/sketch3.js
+++ p5/sketch3.js
@@ -1,6 +1,6 @@
 const count = 32
-const velocity = Math.PI / 18000
-let halfHeight, size, x, y
+const velocity = 1 / (11 * count)
+let halfHeight, size, x, y, a
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
@@ -8,18 +8,17 @@ function setup() {
   size = width / count
   halfSize = size / 2
   halfHeight = windowHeight / 2
-  angle = 0
+  a = 0
 }
 
 function draw() {
   background(0)
   fill(191)
-
+  a += velocity
   for (x = halfSize; x <= width - halfSize; x += size) {
-    // 1. sin((frameCount / period) * Math.PI * 2) — to set velocity by a period
+    // 1. sin(a * Math.PI * 2) — to set velocity by a period
     // 2. x / width — to get offset for each particle
-    angle += velocity
-    y = map( sin((angle - x / width / 2) * Math.PI * 2), -1, 1, halfSize, height - halfSize )
+    y = map( sin((a - x / width / 2) * Math.PI * 2), -1, 1, halfSize, height - halfSize )
     circle(x, y, size)
   }
 }
