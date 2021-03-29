81074432b1582e5f663e657b3f5c98eb56375e88
Ecospell <foretoo@yandex.ru>
2021-03-29 13:15:37 +0300

    refactored

--- p5/simple-pendulum.js
+++ p5/simple-pendulum.js
@@ -1,27 +1,32 @@
 let origin,
     bob,
     arm,
-    angle = Math.PI / 4,
+    aPos = Math.PI / 3,
     aVel = 0.0,
-    aAcc = 0.0
+    aAcc = 0.0,
+    c = -0.667,
+    friction = 0.997
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
   noStroke()
   origin = new p5.Vector(width/2, 0)
-  bob = new p5.Vector(origin.x, arm)
   arm = height / 2
+  bob = new p5.Vector(origin.x + sin(aPos) * arm, origin.y + cos(aPos) * arm)
+}
+
+function update() {
+  aAcc = (c / arm) * sin(aPos) // from pendulum force formula
+  aVel += aAcc
+  aVel *= friction
+  aPos += aVel
+
+  bob.x = origin.x + sin(aPos) * arm
+  bob.y = origin.y + cos(aPos) * arm
 }
 
 function draw() {
-  aAcc = (-0.75 / arm) * sin(angle)
-  aVel += aAcc
-  aVel *= 0.997
-  angle += aVel
-
-  bob.x = origin.x + sin(angle) * arm
-  bob.y = origin.y + cos(angle) * arm
-
+  update()
   background(0)
   fill(191)
   circle(bob.x, bob.y, 20)
@@ -31,6 +36,5 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
-  origin = p5.Vector(width/2, 0)
-  bob = p5.Vector(origin.x, arm)
+  origin.x = width/2
 }
