c14515aeafe5c30b4667ad720ad89ce28def9a9b
Sergey Borovikov <serg.there@gmail.com>
2022-03-20 22:27:23 +0300

    back to fast renderable shapes

--- p5/handulumies/swarm.js
+++ p5/handulumies/swarm.js
@@ -1,6 +1,6 @@
 import "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.1/p5.min.js"
 import { Hand } from "./hand.js"
-import { redraw, play, num, handdraw } from "./gui.js"
+import { redraw, play, num, handdraw, range } from "./gui.js"
 import { clamp, random, random_hex, assign } from "../utils.js"
 
 let hands = [],
@@ -12,8 +12,8 @@ let hands = [],
     bg = "#eee",
     max_weight = 128,
     min_weight = 8,
-    line_length = 128,
-    noise_offset = 0.000
+    line_length = 128
+    // noise_offset = 0.000
 
 
 
@@ -27,10 +27,10 @@ const init_hands = (hands_num) => {
       points: [],
       color: {
         hex: "#" + random_hex(3),
-        h: random(0, 360)  | 0,
-        s: random(25, 100) | 0,
-        b: random(25, 100) | 0,
-        hues: [],
+        // h: random(0, 360)  | 0,
+        // s: random(25, 100) | 0,
+        // b: random(25, 100) | 0,
+        // hues: [],
       },
       weight: random(min_weight, max_weight) | 0,
 
@@ -46,7 +46,7 @@ window.setup = function() {
   createCanvas(windowWidth, windowHeight)
   background(bg)
   noFill()
-  colorMode(HSB)
+  // colorMode(HSB)
   // strokeCap(PROJECT)
   // strokeJoin(ROUND)
   hands = init_hands(parseInt(num.value))
@@ -56,24 +56,28 @@ window.setup = function() {
 
 ////////-- DRAW --////////
 window.draw = function() {
-  // noFill()
-  // strokeCap(ROUND)
-  // strokeJoin(ROUND)
+  noFill()
+  strokeCap(PROJECT)
+  strokeJoin(ROUND)
   background(bg)
 
-  const shift = noise(noise_offset)
+  // const shift = noise(noise_offset)
   hands.forEach(({ hand, points, color, weight }) => {
-    strokeWeight(weight)
-    const hue = (color.h + shift * 360 | 0) % 360
     assign(hand.point, points, line_length)
-    assign(hue, color.hues, line_length)
-    for (let i = 0; i < points.length - 1; i++) {
-      stroke(color.hues[i], color.s, color.b)
-      line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y)
-    }
+    strokeWeight(weight)
+    // const hue = (color.h + shift * 360 | 0) % 360
+    // assign(hue, color.hues, line_length)
+    // for (let i = 0; i < points.length - 1; i++) {
+    //   stroke(color.hues[i], color.s, color.b)
+    //   line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y)
+    // }
+    stroke(color.hex)
+    beginShape()
+    points.forEach(point => vertex(point.x, point.y))
+    endShape()
     hand.update()
   })
-  noise_offset += 0.003
+  // noise_offset += 0.003
 
   if (is_hand_display) hands.forEach(({ hand }) => hand.draw())
 }
@@ -81,6 +85,7 @@ window.draw = function() {
 
 
 ////////-- ADDITIONS --////////
+range.remove()
 handdraw.onchange = () => {
   is_hand_display = !is_hand_display
   if (is_hand_display) handdraw.setAttribute("checked", null)
