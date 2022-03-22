240f18cc40f1940182c1616d64ef9e1ace2135f2
Sergey Borovikov <serg.there@gmail.com>
2022-03-22 14:56:22 +0300

    get more controlable and sinable randomer

--- p5/handulumies/arm-sim.js
+++ p5/handulumies/arm-sim.js
@@ -1,3 +1,5 @@
+import { random } from "../utils.js"
+
 export class Arm_sim extends p5.Vector {
   constructor({ parent, child, length, angle, x, y }) {
     super(x, y)
@@ -5,13 +7,14 @@ export class Arm_sim extends p5.Vector {
     this.child = child || null
     this.length = length
     this.angle = angle
-    this.x = this.parent.x + sin(this.angle) * this.length
-    this.y = this.parent.y + cos(this.angle) * this.length
-    this.angle_f = (Math.random() - 0.5) * 0.03
+    this.x = this.parent.x + sin(angle) * length
+    this.y = this.parent.y + cos(angle) * length
+    this.angle_f = random(0.005, 0.015) * (random(-1, 1) > 0 ? 1 : -1)
+    this.angle_f_f = random(0, Math.PI)
   }
 
   update() {
-    this.angle += this.angle_f
+    this.angle += this.angle_f * sin(this.angle_f_f + frameCount * 0.003)
     this.x = this.parent.x + sin(this.angle) * this.length
     this.y = this.parent.y + cos(this.angle) * this.length
   }
@@ -20,6 +23,7 @@ export class Arm_sim extends p5.Vector {
     fill(255)
     noStroke()
     circle(this.x, this.y, 12)
+    !this.parent.length && circle(this.parent.x, this.parent.y, 12)
     strokeCap(ROUND)
     strokeWeight(24)
     stroke("#fff3")
