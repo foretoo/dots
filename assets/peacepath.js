03883d9cd19928b21f91e40d901ce53008b6da71
Sergey Borovikov <serg.there@gmail.com>
2022-03-24 21:35:43 +0300

    got more readable

--- assets/peacepath.js
+++ assets/peacepath.js
@@ -1,4 +1,5 @@
 export const getpeacepath = (width, height) => {
+  const min = Math.min(width, height)
   const data = `
     M950 500c0 248.53-201.47 450-450 450s-450-201.47-450-450 201.47-450 450-450 450 201.47 450 450z
     m-510-324.56c-153.61 28.216-270 162.8-270 324.56 0 38.695 6.66 75.834 18.896 110.33l251.1-144.98v-289.92z
@@ -6,16 +7,20 @@ export const getpeacepath = (width, height) => {
     m390-103.92c0 38.695-6.66 75.834-18.896 110.33l-251.1-144.98v-289.92c153.61 28.216 270 162.8 270 324.56z
     m-270 103.92 191.03 110.29c-48.186 56.414-115.06 96.388-191.03 110.34v-220.64z
   `
-  const regfloat = /[0-9.]+/g
-  const min = Math.min(width, height)
-  const scale = f => parseFloat(f) * (min / 1000)
 
-  return data.replace(regfloat, scale)
-    .replace(/M[0-9. ]+/g, moveto => (
-      moveto.replace(regfloat, (f, i) => (
-        i === 1
-          ? parseFloat(f) + (width - min)  / 2
-          : parseFloat(f) + (height - min) / 2
-      ))
+  const regfloat = /[0-9.]+/g
+  const scalefloat = f => parseFloat(f) * (min / 1000)
+
+  const regstart = /M[0-9. ]+/g
+  const scalestart = s => (
+    s.replace(regfloat, (f, i) => (
+      i === 1
+        ? parseFloat(f) + (width - min)  / 2
+        : parseFloat(f) + (height - min) / 2
     ))
+  )
+
+  return data
+    .replace(regfloat, scalefloat)
+    .replace(regstart, scalestart)
 }
