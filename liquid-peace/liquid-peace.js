aa03ea405b1666a78524c6afe715a9bf003890c8
Sergey Borovikov <serg.there@gmail.com>
2022-03-25 03:48:16 +0300

    another reconfig

--- liquid-peace/liquid-peace.js
+++ liquid-peace/liquid-peace.js
@@ -5,21 +5,22 @@ import { getpeacepath } from "../assets/peacepath.js"
 import { clamp, random, isFocused } from "../utils.js"
 
 let p5Canvas,
-    ctx,
-    hands = [],
-    arm_num = 4,
-
-    max_weight = 384,
-    min_weight = 96
+    ctx
 
 const feGaussianBlur = document.querySelector("feGaussianBlur"),
-      stdDeviation   = feGaussianBlur.getAttribute("stdDeviation")
+      stdDeviation   = feGaussianBlur.getAttribute("stdDeviation"),
+
+      hands = [],
+      arm_num = 4,
+      hand_num = 32,
+
+      max_weight = 320,
+      min_weight = 64
 
 
 
-const init_hands = (hand_num) => {
-  const hands = []
-  const delta_y = height / (hand_num * 1.5)
+const init_hands = () => {
+  const delta_y = height / hand_num
   const delta_x = width > height ? (width - height) / (hand_num * 1.5) : 0
   const delta_w = Math.sqrt(width * height) / 1000
   for (let i = 0; i < hand_num; i++) {
@@ -34,15 +35,14 @@ const init_hands = (hand_num) => {
         },
       }),
       color: {
-        h: random(45, 55),
-        s: 100,
-        b: random(80, 100),
+        h: random(0, 360),
+        s: random(0, 75),
+        b: 100,
       },
       weight: random(min_weight, max_weight) * delta_w,
 
     })
   }
-  return hands
 }
 
 
@@ -50,7 +50,7 @@ const init_hands = (hand_num) => {
 ////////-- SETUP --////////
 window.setup = function() {
   p5Canvas = createCanvas(windowWidth, windowHeight)
-  p5Canvas.style("filter", "url('#goo')")
+  p5Canvas.style("filter", "url('#liquid')")
   ctx = drawingContext
   ctx.clip(new Path2D(getpeacepath(width, height)), "evenodd")
   feGaussianBlur.setAttribute("stdDeviation", stdDeviation * Math.min(width, height) / 1000)
@@ -58,7 +58,7 @@ window.setup = function() {
   noStroke()
   colorMode(HSB)
 
-  hands = init_hands(parseInt(gui.handnum.value))
+  init_hands()
 }
 
 
@@ -71,7 +71,7 @@ window.draw = function() {
   const shift = sin(frameCount * 0.002)
   hands.forEach(({ hand, color, weight }, i) => {
     const sign = i % 2 == 0 ? 1 : -1
-    const hue = (color.h + shift * sign * 10) % 360
+    const hue = (color.h + shift * sign * 60) % 360
     fill(hue, color.s, color.b)
     circle(hand.point.x, hand.point.y, weight)
 
@@ -89,7 +89,7 @@ const gui = getGUI(
   // { type: "stats" },
   { type: "button", name: "reset" },
   { type: "button", name: "play" },
-  { type: "number", name: "handnum", min: 0, value: 32 },
+  // { type: "number", name: "handnum", min: 0, value: 32 },
 )
 gui.play.textContent = "stop"
 const toggle_play = () => {
@@ -105,11 +105,12 @@ const toggle_play = () => {
 const handle_reset = () => {
   p5Canvas.remove()
   p5Canvas = createCanvas(windowWidth, windowHeight)
-  p5Canvas.style("filter", "url('#goo')")
+  p5Canvas.style("filter", "url('#liquid')")
   ctx.clip(new Path2D(getpeacepath(width, height)), "evenodd")
   feGaussianBlur.setAttribute("stdDeviation", stdDeviation * Math.min(width, height) / 1000)
 
-  hands = init_hands(parseInt(gui.handnum.value))
+  hands.length = 0
+  init_hands()
   if (!isLooping()) {
     gui.play.textContent = "stop"
     loop()
@@ -119,10 +120,10 @@ const handle_reset = () => {
 //   if (handdraw.checked) handdraw.removeAttribute("checked")
 //   else handdraw.setAttribute("checked", null)
 // }
-gui.handnum.oninput = () => {
-  gui.handnum.value = clamp(parseInt(gui.handnum.value), 1, 48)
-  handle_reset()
-}
+// gui.handnum.oninput = () => {
+//   gui.handnum.value = clamp(parseInt(gui.handnum.value), 1, 48)
+//   handle_reset()
+// }
 gui.reset.onclick = handle_reset
 gui.play.onclick = toggle_play
 document.onkeyup = ({ code }) => {
