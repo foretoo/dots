a7290d5c6a34a80ca3224de51acd03aef44b4fbb
Sergey Borovikov <serg.there@gmail.com>
2022-04-08 11:43:35 +0300

    add margin

--- poisson-disc/poisson.js
+++ poisson-disc/poisson.js
@@ -3,7 +3,8 @@ import psd from "https://cdn.skypack.dev/poisson-disk-sampling@2.2.3";
 const colors = [ "#fb7", "#4a7", "#258" ],
       minDistance = 20,
       maxDistance = 20,
-      radius = 20
+      radius = 20,
+      margin = 40
 
 
 
@@ -20,12 +21,19 @@ window.setup = function() {
 
 function drawPoints() {
   new psd({
-    shape: [ width - minDistance, height - minDistance ],
+    shape: [
+      width - minDistance - margin * 2,
+      height - minDistance - margin * 2
+    ],
     minDistance,
     maxDistance,
   }).fill().forEach(point => {
     fill(random(colors))
-    circle(point[0] + minDistance / 2, point[1] + minDistance / 2, radius)
+    circle(
+      point[0] + margin + minDistance / 2,
+      point[1] + margin + minDistance / 2,
+      radius
+    )
   })
 }
 
