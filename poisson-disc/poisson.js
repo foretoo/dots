bb21625b3436ac843f12c5f31dba00e2b9e6123b
Sergey Borovikov <serg.there@gmail.com>
2022-05-04 22:20:27 +0300

    add noise

--- poisson-disc/poisson.js
+++ poisson-disc/poisson.js
@@ -1,44 +1,49 @@
-import psd from "https://cdn.skypack.dev/poisson-disk-sampling@2.2.3";
+import psd from "https://cdn.skypack.dev/poisson-disk-sampling@2.2.3"
+import { get_noiser } from "../assets/noise.js"
 
-const colors = [ "#fb7", "#4a7", "#258" ],
-      minDistance = 20,
-      maxDistance = 20,
-      radius = 20,
-      margin = 40
 
+const minDistance = 12,
+      maxDistance = 72,
+      radius = 12,
+      margin = 40,
+      nscale = 0.01
 
 
 ////////-- SETUP --////////
 window.setup = function() {
   createCanvas(windowWidth, windowHeight)
-  background("#111")
+  background(255)
   noStroke()
   noLoop()
-  
 
-  drawPoints()
+  getPoints().forEach(drawCircle)
 }
 
-function drawPoints() {
-  new psd({
+
+function drawCircle(p) {
+  const x = marginPoint(p[0]),
+        y = marginPoint(p[1]),
+        f = get_noiser(p[0], p[1], nscale),
+        size = radius * Math.pow(f, 1.1) * 5,
+        bit = (1 - Math.sqrt(f)) * 255
+
+  fill(255, bit, bit)
+  circle(x, y, size)
+}
+
+function getPoints() {
+  return new psd({
     shape: [
-      width - minDistance - margin * 2,
-      height - minDistance - margin * 2
+      marginSize(width),
+      marginSize(height)
     ],
     minDistance,
     maxDistance,
-  }).fill().forEach(point => {
-    fill(random(colors))
-    circle(
-      point[0] + margin + minDistance / 2,
-      point[1] + margin + minDistance / 2,
-      radius
+    distanceFunction: (p) => (
+      get_noiser(p[0], p[1], nscale)
     )
-  })
+  }).fill()
 }
 
-
-
-////////-- DRAW --////////
-// window.draw = function() {
-// }
+const marginSize = (size) => size - minDistance - margin * 2
+const marginPoint = (point) => point + margin + minDistance / 2
