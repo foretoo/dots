ed64bcacc88abca94e298a2bc4eaadb2257b00a8
Sergey Borovikov <serg.there@gmail.com>
2022-12-02 12:21:04 +0000

    fix noise

--- assets/noise.js
+++ assets/noise.js
@@ -2,13 +2,18 @@ import { createNoise2D } from "https://cdn.skypack.dev/simplex-noise"
 
 const noise2D = createNoise2D()
 
-export const get_noise = (x = 0, y = 0) => (
-  ( noise2D(x, y) + 1 ) / 2
+export const get_noise = (
+  x = 0,
+  y = 0
+) => (
+  noise2D(x, y) * 0.5 + 0.5
 )
-export const get_noiser = (x = 0, y = 0, scale = 1) => (
-  Math.pow(
-    get_noise(x * scale / 3, y * scale / 3) * 1.3 +
-    get_noise(x * scale, y * scale) * 0.7,
-    2
-  ) / 4
+
+export const get_noiser = (
+  x = 0,
+  y = 0,
+  scale = 1
+) => (
+  get_noise(x * scale * 0.33333333, y * scale * 0.33333333) *
+  get_noise(x * scale, y * scale)
 )
